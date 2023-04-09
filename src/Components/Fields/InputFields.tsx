4394e94455954d3241c37567704d5f427f9bb30e
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-04-10 01:26:06 +0300

    fix (InputFields) reactor InputFields.tsx classes grid. added props

--- src/Components/Fields/InputFields.tsx
+++ src/Components/Fields/InputFields.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import {ButtonsForFields} from "./BattonsForFields/ButtonsForFields";
+import React, {FC} from 'react';
 
-export const InputFields = () => {
+interface InputFieldsType {
+    changeText1: string
+    changeText2: string
+    handleChange1: (event: React.ChangeEvent<HTMLInputElement>) => void
+    handleChange2: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const InputFields: FC<InputFieldsType> = ({
+                                                     changeText1,
+                                                     changeText2,
+                                                     handleChange1,
+                                                     handleChange2
+                                                 }) => {
+
     return (
-        <div>
-            <input placeholder={"Переменная"}/>
-            <input placeholder={"Значение"}/>
-            <ButtonsForFields/>
+        <div className="mt-5 row">
+            <div className="col-12 col-md-6">
+                <input type="text" value={changeText1} onChange={handleChange1} className="form-control" placeholder="Имя переменной:" />
+            </div>
+            <div className="col-12 col-md-6 mt-3 mt-md-0">
+                <input type="text" value={changeText2} onChange={handleChange2} className="form-control" placeholder="Значение переменной:" />
+            </div>
+            {/*<ButtonsForFields/>*/}
         </div>
     );
 };
