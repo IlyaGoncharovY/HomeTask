67c335703263c93423c9ac2b6605189dc8477371
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-04-10 23:21:56 +0300

    fix (InputFields) use rafactoring hook

--- src/Components/Fields/InputFields.tsx
+++ src/Components/Fields/InputFields.tsx
@@ -1,29 +1,45 @@
-import React, {FC} from 'react';
+import React, {ChangeEvent, FC} from 'react';
+import {ButtonsForFields} from "./BattonsForFields/ButtonsForFields";
 
 interface InputFieldsType {
-    changeText1: string
-    changeText2: string
-    handleChange1: (event: React.ChangeEvent<HTMLInputElement>) => void
-    handleChange2: (event: React.ChangeEvent<HTMLInputElement>) => void
+    changeTexts: { variable: string; valueVariable: string; }[]
+    handleChangeText: (index: number, field: "variable" | "valueVariable") => (e: ChangeEvent<HTMLInputElement>) => void
+    handleAddPair: () => void
+    handleDeletePair: () => void
 }
 
 export const InputFields: FC<InputFieldsType> = ({
-                                                     changeText1,
-                                                     changeText2,
-                                                     handleChange1,
-                                                     handleChange2
+                                                     changeTexts,
+                                                     handleChangeText,
+                                                     handleAddPair,
+                                                     handleDeletePair
                                                  }) => {
 
     return (
-        <div className="mt-5 row">
-            <div className="col-12 col-md-6">
-                <input type="text" value={changeText1} onChange={handleChange1} className="form-control" placeholder="Имя переменной:" />
-            </div>
-            <div className="col-12 col-md-6 mt-3 mt-md-0">
-                <input type="text" value={changeText2} onChange={handleChange2} className="form-control" placeholder="Значение переменной:" />
-            </div>
-            {/*<ButtonsForFields/>*/}
-        </div>
+        <>
+            {changeTexts.map((changeText, index) => (
+                <div key={index} className="mt-5 row">
+                    <div className="col-12 col-md-6">
+                        <input
+                            type="text"
+                            value={changeText.variable}
+                            onChange={handleChangeText(index, 'variable')}
+                            placeholder={"Имя переменной"}
+                        />
+                    </div>
+                    <div className="col-12 col-md-6 mt-3 mt-md-0">
+                        <input
+                            type="text"
+                            value={changeText.valueVariable}
+                            onChange={handleChangeText(index, 'valueVariable')}
+                            placeholder={"Значение переменной:"}
+                        />
+                    </div>
+                </div>
+            ))}
+            <ButtonsForFields handleAddPair={handleAddPair}
+                              handleDeletePair={handleDeletePair}/>
+        </>
     );
 };
 
