19e38003d4aa09a943a410f919db2604f94f4389
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-04-11 21:57:23 +0300

    fix (DownloadFile) added function for added default file Word

--- src/Components/DownloadButton/DownloadButton.tsx
+++ src/Components/DownloadButton/DownloadButton.tsx
@@ -1,4 +1,7 @@
 import React, {FC} from 'react';
+// @ts-ignore
+import file from "../../Assets/baseWordFile/TestWord2.docx"
+
 
 interface DownloadButtonType {
     docxFile: File | null
@@ -8,10 +11,28 @@ interface DownloadButtonType {
 
 export const DownloadButton: FC<DownloadButtonType> = ({docxFile, handleFileDownload}) => {
 
+    const onDownload = () => {
+        fetch(file).then((response) => {
+            response.blob().then((blob) => {
+                let url = window.URL.createObjectURL(blob);
+                let a = document.createElement("a");
+                a.href = url;
+                a.download = "testFile.docx";
+                a.click();
+            });
+        });
+    };
+
     return (
-        <div className={"col-12 mt-2 text-end"}>
-            <button type="button" className="btn btn-success" onClick={handleFileDownload} disabled={!docxFile}>Скачать изменённый документ</button>
-        </div>
+        <>
+            <div className={"col-12 mt-2 text-end"}>
+                <button type="button" className="btn btn-outline-info" onClick={onDownload}>Скачать дефолтный файл</button>
+            </div>
+            <div className={"col-12 mt-2 text-end"}>
+                <button type="button" className="btn btn-success" onClick={handleFileDownload} disabled={!docxFile}>Скачать изменённый документ</button>
+            </div>
+        </>
+
     );
 };
 
